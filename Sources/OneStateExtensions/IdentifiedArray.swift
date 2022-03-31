157755b7f40ea967fcbd0be1c11941e57ca70ae1
Måns Bernhardt <mans.bernhardt@apple.com>
2022-03-31 20:47:46 +0200

    Updated IdentifiedArray extensions to use more efficient key path member lookup via new Cursor helper.

--- Sources/OneStateExtensions/IdentifiedArray.swift
+++ Sources/OneStateExtensions/IdentifiedArray.swift
@@ -7,7 +7,7 @@ public extension StoreViewProvider {
             lhs.ids == rhs.ids
         }
         
-        return value(for: keyPath, isSame: isSame).ids.compactMap { id in
+        return value(for: keyPath, shouldUpdateViewModelAccessToViewAccess: true, isSame: isSame).ids.compactMap { id in
             storeView(for: keyPath.appending(path: \IdentifiedArray<Id, Element>[id: id]))
         }
     }
@@ -16,13 +16,15 @@ public extension StoreViewProvider {
         func isSame(lhs: IdentifiedArray<Element.ID, Element>, rhs: IdentifiedArray<Element.ID, Element>) -> Bool {
             lhs.ids == rhs.ids
         }
-
-        let array = value(for: keyPath, isSame: isSame)
-        let views = array.ids.compactMap { id in
-            storeView(for: keyPath.appending(path: \IdentifiedArray<Element.ID, Element>[id: id]))
-        }
+        
+        let array = value(for: keyPath, shouldUpdateViewModelAccessToViewAccess: true, isSame: isSame)
         let idPath = \StoreView<Root, Element>.id
-        return IdentifiedArray(uniqueElements: views, id: idPath)
+
+        return IdentifiedArray(uniqueElements: array.indices.map { index in
+            let element = array[index]
+            let path = \IdentifiedArray<Element.ID, Element>[cursor: Cursor(id: element[keyPath: array.id], index: index, fallback: element)]
+            return storeView(for: keyPath.appending(path: path))
+        }, id: idPath)
     }
 }
 
@@ -32,5 +34,37 @@ public extension IdentifiedArray {
             uniqueElements: self.elements.map(transform),
             id: \.id
         )
+    }
+}
+
+extension IdentifiedArray {
+    fileprivate subscript(cursor cursor: Cursor<ID, Element>) -> Element {
+        get {
+            if cursor.index >= startIndex && cursor.index < endIndex {
+                let element = self[cursor.index]
+                if element[keyPath: id] == cursor.id {
+                    return element
+                }
+            }
+               
+            return self[id: cursor.id] ?? cursor.fallback
+        }
+        set {
+            self[id: cursor.id] = newValue
+        }
+    }
+}
+
+private struct Cursor<ID: Hashable, Element>: Hashable {
+    var id: ID
+    var index: IdentifiedArray<ID, Element>.Index
+    var fallback: Element
+    
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
     }
 }
