4474c79f340456191284f0c010bda93cca107c44
Måns Bernhardt <mans.bernhardt@gmail.com>
2022-07-16 21:24:46 +0200

    Extracted assertNoFailure using XCTestDynamicOverlay to separate free function.

--- Sources/OneStateExtensions/XCTestDynamicOverlay.swift
+++ Sources/OneStateExtensions/XCTestDynamicOverlay.swift
@@ -2,52 +2,55 @@ import OneState
 import XCTestDynamicOverlay
 import CustomDump
 
+@Sendable
+public func assertNoFailure<State>(failure: TestFailure<State>) {
+    switch failure.kind {
+    case let .assertStateMismatch(expected: expected, actual: actual):
+        let difference = diff(expected, actual, format: .proportional)
+            .map { "\($0.indent(by: 4))\n\n(Expected: −, Actual: +)" }
+        ??  """
+            Expected:
+            \(String(describing: expected).indent(by: 2))
+            Actual:
+            \(String(describing: actual).indent(by: 2))
+            """
+
+        XCTFail(
+            """
+            State change does not match expectation: …
+            \(difference)
+            """,
+            file: failure.file,
+            line: failure.line
+        )
+
+    case let .stateNotExhausted(lastAsserted: lastAsserted, actual: actual):
+        let difference = diff(lastAsserted, actual, format: .proportional)
+            .map { "\($0.indent(by: 4))\n\n(Last asserted: −, Actual: +)" }
+        ??  """
+            Last asserted:
+            \(String(describing: lastAsserted).indent(by: 2))
+            Actual:
+            \(String(describing: actual).indent(by: 2))
+            """
+
+        XCTFail(
+            """
+            State not exhausted: …
+            \(difference)
+            """,
+            file: failure.file,
+            line: failure.line
+        )
+
+    default:
+        XCTFail(failure.message, file: failure.file, line: failure.line)
+    }
+}
+
 public extension TestStore {
     convenience init(initialState: State, environments: [Any] = []) {
-        self.init(initialState: initialState, environments: environments) { @MainActor failure in
-            switch failure.kind {
-            case let .assertStateMismatch(expected: expected, actual: actual):
-                let difference = diff(expected, actual, format: .proportional)
-                    .map { "\($0.indent(by: 4))\n\n(Expected: −, Actual: +)" }
-                ??  """
-                    Expected:
-                    \(String(describing: expected).indent(by: 2))
-                    Actual:
-                    \(String(describing: actual).indent(by: 2))
-                    """
-
-                XCTFail(
-                    """
-                    State change does not match expectation: …
-                    \(difference)
-                    """,
-                    file: failure.file,
-                    line: failure.line
-                )
-
-            case let .stateNotExhausted(lastAsserted: lastAsserted, actual: actual):
-                let difference = diff(lastAsserted, actual, format: .proportional)
-                    .map { "\($0.indent(by: 4))\n\n(Last asserted: −, Actual: +)" }
-                ??  """
-                    Last asserted:
-                    \(String(describing: lastAsserted).indent(by: 2))
-                    Actual:
-                    \(String(describing: actual).indent(by: 2))
-                    """
-
-                XCTFail(
-                    """
-                    State not exhausted: …
-                    \(difference)
-                    """,
-                    file: failure.file,
-                    line: failure.line
-                )
-
-            default:
-                XCTFail(failure.message)
-            }
-        }
+        self.init(initialState: initialState, environments: environments, onTestFailure: assertNoFailure)
     }
 
     convenience init<T>(initialState: T, environments: [Any] = []) where M == EmptyModel<T> {
