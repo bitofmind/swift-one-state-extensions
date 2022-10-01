c443572ca80542e5f4df5a02f6ad26e2f73b156b
Måns Bernhardt <mans.bernhardt@gmail.com>
2022-10-01 09:32:28 +0200

    Make sure IdentifiedArray reports path fallbacks,

--- Sources/OneStateExtensions/IdentifiedArray.swift
+++ Sources/OneStateExtensions/IdentifiedArray.swift
@@ -66,7 +66,11 @@ private extension IdentifiedArray {
                 }
             }
                
-            return self[id: cursor.id] ?? cursor.fallback
+            guard let value = self[id: cursor.id] else {
+                reportPathDidFallback()
+                return cursor.fallback
+            }
+            return value
         }
         set {
             cursor.fallback = newValue
